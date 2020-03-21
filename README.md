a509702b028ac216d4c23979eede3949fa940cda
simon-macbook <simon.day91@hotmail.co.uk>
2020-03-21 13:33:15 +0800

    double click to add new marker set up with different marker icon

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Log Entry
 
-- Title
-- Description
-- Image
-- Start Date
-- End Date
-- Latitude
-- Longitude
+- Title - Text
+- Description - Text
+- Rating - scale of 1 - 10
+- Image - Text - URL
+- Start Date - DateTime
+- End Date - DateTime
+- Latitude - Number
+- Longitude - Number
+- Created At - DateTime
+- Updated At - DateTime
---
+++ client/src/App.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { listLogEntries } from './API';
+
+const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
+
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 41.1277557,
+    longitude: -100.7946784,
+    zoom: 3
+  });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      console.log(logEntries);
+      setLogEntries(logEntries);
+    })();
+  }, []);
+
+  const showAddMarkerPopup = e => {
+    const [longitude, latitude] = e.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude
+    });
+  };
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onDblClick={showAddMarkerPopup}
+      mapStyle="mapbox://styles/simondtw/ck7txpw8e00ko1ilaurz60ole"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={setViewport}
+    >
+      {logEntries.map(logEntry => (
+        <React.Fragment key={logEntry._id}>
+          <Marker latitude={logEntry.latitude} longitude={logEntry.longitude}>
+            <div
+              onClick={() => {
+                setShowPopup({
+                  [logEntry._id]: true
+                });
+              }}
+            >
+              <img
+                style={{
+                  height: `${8 * viewport.zoom}px`,
+                  width: `${8 * viewport.zoom}px`
+                }}
+                className="marker"
+                src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png"
+                alt="marker"
+              />
+            </div>
+          </Marker>
+          {showPopup[logEntry._id] ? (
+            <Popup
+              dynamicPosition={true}
+              latitude={logEntry.latitude}
+              longitude={logEntry.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => setShowPopup({})}
+              anchor="top"
+            >
+              <div className="popup">
+                <h3>{logEntry.title}</h3>
+                <p>{logEntry.comments}</p>
+              </div>
+            </Popup>
+          ) : null}
+        </React.Fragment>
+      ))}
+      {addEntryLocation ? (
+        <>
+          <Marker
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+          >
+            <div>
+              <img
+                style={{
+                  height: `${8 * viewport.zoom}px`,
+                  width: `${8 * viewport.zoom}px`
+                }}
+                className="marker"
+                src="https://image.flaticon.com/icons/png/512/37/37134.png"
+                alt="marker"
+              />
+            </div>
+          </Marker>
+          <Popup
+            dynamicPosition={true}
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setAddEntryLocation(null)}
+            anchor="top"
+          >
+            <div className="popup">
+              <h3>Add Your New Entry Here:</h3>
+            </div>
+          </Popup>
+        </>
+      ) : null}
+    </ReactMapGL>
+  );
+};
+
+export default App;
